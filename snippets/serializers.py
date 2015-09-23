a080e76ba6eaf0c79fbe2da951eef841f451e6ac
cwalsh <cwalsh@leapfrogonline.com>
2015-09-23 13:22:25 -0500

    creates class-based views, adds authentication and permissions, and sets up hyperlinked api

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,25 @@
-from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import permissions, serializers
+
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
-    
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'highlight', 'owner', 'title', 'code', 'linenos',
+                  'language', 'style')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True,
+                                                   view_name='snippet-detail',
+                                                   read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
